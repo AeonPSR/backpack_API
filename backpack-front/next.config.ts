21de7dd9a1a74eea0f92306b04cf49b42febe156
AeonPSR <floriandevigne1@hotmail.fr>
2025-02-04 09:34:51 -0500

    Same

--- backpack-front/next.config.ts
+++ backpack-front/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  experimental: {
-    outputStandalone: true,
-  },
-  serverRuntimeConfig: {
-    host: "0.0.0.0",
-    port: 3000
-  }
+  output: "standalone", 
 };
 
 export default nextConfig;
