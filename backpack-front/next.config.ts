850f693fa05acc69ef20df8d612a415b3db9b359
AeonPSR <floriandevigne1@hotmail.fr>
2025-02-04 09:01:29 -0500

    Mod next js config to allow external connection

--- backpack-front/next.config.ts
+++ backpack-front/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	//
-  };
+  output: "standalone",
+  experimental: {
+    outputStandalone: true,
+  },
+  serverRuntimeConfig: {
+    host: "0.0.0.0",
+    port: 3000
+  }
+};
 
 export default nextConfig;
